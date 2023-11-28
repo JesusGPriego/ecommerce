2e45dbf12072d357de75e7f14ba063d647bea2cd
Jesus G. Priego <70105185+JesusGPriego@users.noreply.github.com>
2023-11-28 13:49:31 +0100

    Validación cliente

--- src/main/java/org/suleware/ecommerce/ecommerce/models/entity/Cliente.java
+++ src/main/java/org/suleware/ecommerce/ecommerce/models/entity/Cliente.java
@@ -11,6 +11,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "clientes")
@@ -19,9 +22,14 @@ public class Cliente implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
     private String nombre;
+    @NotEmpty
     private String apellido;
+    @NotEmpty
+    @Email
     private String email;
+    @NotNull
     @Column(name = "created_at")
     @Temporal(TemporalType.DATE)
     private Date createdAt;
