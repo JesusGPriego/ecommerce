8d3f0b8997dff852cc2662e088619a2a7ed8a146
Jesus G. Priego <70105185+JesusGPriego@users.noreply.github.com>
2023-12-05 15:54:07 +0100

    validations

--- src/main/java/org/suleware/ecommerce/ecommerce/controller/AppController.java
+++ src/main/java/org/suleware/ecommerce/ecommerce/controller/AppController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.suleware.ecommerce.ecommerce.models.dao.IClienteDao;
 import org.suleware.ecommerce.ecommerce.models.entity.Cliente;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class AppController {
@@ -36,7 +39,13 @@ public class AppController {
     }
 
     @PostMapping("/form")
-    public String guardar(Cliente cliente) {
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", "Formulario de cliente");
+            return "form";
+        }
+
         clienteDao.save(cliente);
         return "redirect:/listar";
     }
