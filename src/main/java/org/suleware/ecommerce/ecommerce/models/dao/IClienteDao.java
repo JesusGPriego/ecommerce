c0dc05686a7194dd399ef476be81dc9bbeac1fd7
Jesus G. Priego <70105185+JesusGPriego@users.noreply.github.com>
2023-12-06 16:02:35 +0100

    dao to jpa repository

--- src/main/java/org/suleware/ecommerce/ecommerce/models/dao/IClienteDao.java
+++ src/main/java/org/suleware/ecommerce/ecommerce/models/dao/IClienteDao.java
@@ -1,15 +1,7 @@
 package org.suleware.ecommerce.ecommerce.models.dao;
 
-import java.util.List;
-
+import org.springframework.data.repository.CrudRepository;
 import org.suleware.ecommerce.ecommerce.models.entity.Cliente;
 
-public interface IClienteDao {
-    public List<Cliente> findAll();
-
-    public void save(Cliente cliente);
-
-    public Cliente findOne(Long id);
-
-    public void delete(Long id);
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
 }
