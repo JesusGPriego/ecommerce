2b886ea9dca676e39b03cb9ac8a907bb3ed3f9b9
Jesus G. Priego <70105185+JesusGPriego@users.noreply.github.com>
2023-12-14 18:33:50 +0100

    update

--- src/main/java/org/suleware/ecommerce/ecommerce/models/service/IClienteService.java
+++ src/main/java/org/suleware/ecommerce/ecommerce/models/service/IClienteService.java
@@ -3,7 +3,6 @@ package org.suleware.ecommerce.ecommerce.models.service;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.suleware.ecommerce.ecommerce.models.entity.Cliente;
 
 public interface IClienteService {
